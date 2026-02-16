941cc1c208b3c18d8e870a6e1ad022ccd80b8732
mj640 <mj640@exeter.ac.uk>
2026-02-16 12:39:02 +0000

    Task 1.2

--- CopyArrayApp.java
+++ CopyArrayApp.java
@@ -1,9 +1,21 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class CopyArrayApp{
     public static void main(String[] args) {
         int nVals = args.length;
+        // to create a new array of ints
+        ArrayList<Integer> myVals = new ArrayList<>(nVals);
+        ArrayList<Integer> uniqueVals = new ArrayList<>(nVals);
         for (int i = 0; i < nVals; i++) {
             int item = Integer.parseInt(args[i]);
+            myVals.add(item);
+            if (!uniqueVals.contains(item)) {
+                uniqueVals.add(item);
+            }
             System.out.println(item);
         }
+        System.out.println("original values: " + myVals);
+        System.out.println("unique values: " + uniqueVals);
     }
 }
